fce3c1ec6552f3d4189f956417248d400255a73c
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-13 16:46:41 +0530

    first commit and started server

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "username already existed"],
+        required: [true, "username is required"]
+    },
+    email: {
+        type: String,
+        unique: [true, "email already existed"],
+        required: [true, "email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"]
+    },
+    bio: String,
+    profileImage: {
+        type: String,
+        default: "https://ik.imagekit.io/fpttn101t/avatar-default-user-profile-icon-social-media-vector-57234208.webp"
+    }
+})
